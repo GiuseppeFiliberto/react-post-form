4649e44e83a415f33ecbad22086b71014a2aa5ce
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-31 17:32:44 +0200

    feat: enhance form layout and add input fields for author, title, and body

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ function App() {
 
   return (
     <>
-      <div className="container p-4">
+      <div className="container p-4 bg-light mt-5 rounded">
 
-        <h1 className='text-white'>Post Maker</h1>
+        <h1 className='fw-bold mb-5'>Post Maker</h1>
         <form method='POST'>
           <div className="row">
+
+            {/* author */}
             <div className="col">
               <div className="mb-3">
                 <label for="" className="form-label">Author</label>
@@ -24,7 +26,78 @@ function App() {
                 <small id="helpId" className="form-text text-secondary">Help text</small>
               </div>
             </div>
+
+            {/* title */}
+            <div className="col">
+              <div className="mb-3">
+                <label for="" className="form-label">Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="title"
+                  id=""
+                  aria-describedby="helpId"
+                  placeholder="Type the title"
+                  value={FormData.title}
+                />
+                <small id="helpId" className="form-text text-secondary">Help text</small>
+              </div>
+            </div>
+
+            {/* body */}
+            <div className="col-12">
+              <div class="mb-3">
+                <label forHtml="" class="form-label"></label>
+                <textarea class="form-control" name="body" id="" rows="5" value={FormData.body} placeholder='Write something here'>
+
+                </textarea>
+              </div>
+
+            </div>
+
+            {/* check */}
+
+            <div className='p-3' role="group" data-bs-toggle="buttons">
+              <label
+                class="btn btn-secondary active"
+              >
+                <input
+                  type="checkbox"
+                  class="me-2 mr-5"
+                  name="private"
+                  id=""
+                  checked
+                  autocomplete="off"
+                  value='private'
+                />
+                Private
+              </label>
+
+              <label
+                class="btn btn-primary"
+              >
+                <input
+                  type="checkbox"
+                  class="me-2"
+                  name="public"
+                  id=""
+                  autocomplete="off"
+                  value='public'
+                />
+                Public
+              </label>
+            </div>
+
+
+
           </div>
+
+          <button
+            type="submit"
+            class="btn btn-primary btn-lg"
+          >
+            Submit
+          </button>
 
 
         </form >
